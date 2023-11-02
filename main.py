da0e2515cbb9202d2bebf8839960c2cb721b5913
HienNguyen-ITTC <67691706+HienNguyen-ITTC@users.noreply.github.com>
2023-11-02 08:54:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
-    "http://localhost:8000",
+    "http://localhost:3000",
 ]
 app.add_middleware(
     CORSMiddleware,
